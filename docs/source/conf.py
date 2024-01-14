e825fa6287800818ab1c81ad492be4bb0f170bef
m4thphobia <143798227+m4thphobia@users.noreply.github.com>
2024-01-15 01:30:10 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Almost unsure'
+copyright = '2023, m4thphobia'
+author = 'm4thphobia'
 
 release = '0.1'
 version = '0.1.0'
